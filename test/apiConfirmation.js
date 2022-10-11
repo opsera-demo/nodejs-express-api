8370cf495a2700a3eda966237d790f501a257135
redmed86 <dross91786@gmail.com>
2022-10-11 09:55:12 -0600

    initial commit

---
+++ test/apiConfirmation.js
@@ -0,0 +1,22 @@
+let expect  = require('chai').expect;
+let request = require('request');
+let hostname = process.env.TEST_HOST || 'localhost';
+let port = process.env.TEST_PORT || 3000;
+
+it('Hello World', (done) => {
+    request('http://' + hostname + ':' + port + '/api/javascript' , function(error, response, body) {
+        let json = JSON.parse(body);
+        const msg = json.msg
+        expect(msg).to.equal('Hello World!');
+        done();
+    });
+});
+
+it('API Language', (done) => {
+    request('http://' + hostname + ':' + port + '/api/javascript' , function(error, response, body) {
+        let json = JSON.parse(body);
+        const language = json.language
+        expect(language).to.equal('NodeJS');
+        done();
+    });
+});
