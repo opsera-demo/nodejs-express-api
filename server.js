a4114c564d9591e29b87a3ab410c5e16a59ee361
principal-arc <113467759+principal-arc@users.noreply.github.com>
2023-03-14 10:49:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/api/javascript', function(req, res){
            language: 'NodeJS', 
            date: dateString,
            hostname: host, 
-           customer: 'peter-test'
+           customer: 'peter1-test'
         });
 });
 
